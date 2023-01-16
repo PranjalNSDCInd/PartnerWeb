74aed1428195b3e6fe687af27e7ccc62a5ad0175
Pranjal Prasun <pranjal.prasun@nsdcindia.org>
2023-01-16 11:46:29 +0530

    login html and css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PartnerLoginComponent } from './modules/pages/partner-login/partner-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'partner-login', component: PartnerLoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/pages/partner-login/partner-login.component.ts
+++ src/app/modules/pages/partner-login/partner-login.component.ts
@@ -3,10 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-partner-login',
   templateUrl: './partner-login.component.html',
-  styleUrls: ['./partner-login.component.css']
+  styleUrls: ['./partner-login.component.css', '../../../../../src/app/shared/custom-css/login.css']
 })
 export class PartnerLoginComponent implements OnInit {
 
+  timer = 30;
+  
   constructor() { }
 
   ngOnInit(): void {
